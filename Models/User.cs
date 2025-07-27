8dbe2aef1ead73cf6e2a648cae673099f6a88f22
Matheus Fernandes De Souza Leo <ms961086@gmail.com>
2025-07-27 16:13:38 -0300

    inserindo nome

--- Models/User.cs
+++ Models/User.cs
@@ -3,6 +3,8 @@
     public class User
     {
         public int Id { get; set; }
+
+        public string? Name { get; set; }
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public DateTime? CreatedOn { get; set; }
