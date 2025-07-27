511df8bdf2a46b55d5400fbc188ce03d65468e19
Matheus Fernandes De Souza Leo <ms961086@gmail.com>
2025-07-27 15:36:21 -0300

    Criando tabela de usuarios

---
+++ Models/User.cs
@@ -0,0 +1,17 @@
+﻿namespace Care3._0.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public DateTime? CreatedOn { get; set; }
+        public string? CreatedBy{ get; set; }
+        public DateTime? UpdatedOn { get; set; }
+        public string? UpdatedBy{ get; set; }
+        public DateTime? DeletedOn { get; set; }
+        public string? DeletedBy{ get;set; }
+
+    }
+}
+
